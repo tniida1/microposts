a391eec97f9ca680e7228f5e2bfff0eb23830c50
tniida1 <tniida1@gmail.com>
2015-09-15 16:46:58 +0000

    fix layout #3

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,11 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#home'
   get 'signup',  to: 'users#new'
-<<<<<<< HEAD
   get 'users/:id/following',  to: 'users#following'
   get 'users/:id/follower',  to: 'users#follower'
   get     'login'   => 'sessions#new'  
   post    'login'   => 'sessions#create'  
   delete  'logout'  => 'sessions#destroy'  
-=======
-  get    'login'   => 'sessions#new'  
-  post   'login'   => 'sessions#create'  
-  delete 'logout'  => 'sessions#destroy'  
->>>>>>> user-profile
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
